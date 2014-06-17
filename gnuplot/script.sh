b06aefce2992b4ab90b70caa3f9279809ec979b5
pierre <pierre@localhost.localdomain>
2014-06-17 10:18:39 +0900

    a

--- gnuplot/script.sh
+++ gnuplot/script.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-./trafic measure.dat distrib.dat
- 
-cat measure.dat
-cat distrib.dat
+
+../bin/traffic measure.dat distrib.dat
+
+
 
 gnuplot<<EOF
 set terminal gif medium size 448,336
-set output './graphics.png'
+set output './Distrib.png'
 set xlabel 'Traffic'
-set ylabel 'Probability'
+set ylabel 'Probality'
 set grid
 set nokey
-set title 'Probability of flow'
-plot "data.dat" using 1:8 with lines smooth unique, "data.dat" using 1:7 with lines smooth unique, "data.dat" using 1:6 with lines smooth unique,"data.dat" using 1:5 with lines smooth unique
+set title 'Distribution of probability'
+plot "distrib.dat" using 1:2 with lines smooth unique
+set terminal gif medium size 448,336
+set output './Traffic.png'
+set xlabel 'Time'
+set ylabel 'Traffic' 
+set grid
+set nokey
+set title 'Sum of traffic'
+plot "measure.dat" using 1:2 with lines smooth unique
 EOF
 
 
-display graphics.png
+display Traffic.png & display Distrib.png 
 
 rm measure.dat
+rm distrib.dat
